9b8cf691bb54af1341597ba0a0d333ed7fddce07
Nirav Patel <nirav4273@gmail.com>
2026-09-16 09:23:23 +0530

    Update file with Groq

--- llm.py
+++ llm.py
@@ -1,9 +1,15 @@
 from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from pydantic import SecretStr
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+groq_api_key = SecretStr(os.getenv("GROQ_API_KEY", ''))
+print(groq_api_key)
+if not groq_api_key:
+    raise RuntimeError('Set GROQ_API_KEY first: $env:GROQ_API_KEY="your_api_key"')
 
 openai_api_key = SecretStr(os.getenv("OPENAI_API_KEY", ''))
 if not openai_api_key:
@@ -15,10 +21,10 @@ def load_llm(provider: str):
         return ChatOpenAI(
             model=os.getenv('OPENAI_MODEL', '')
         )
-    # return ChatGroq(
-    #     api_key=groq_api_key,
-    #     model_name=os.getenv('GROQ_MODEL', '')
-    # )
+    return ChatGroq(
+        api_key=groq_api_key,
+        model=os.getenv('GROQ_MODEL', '')
+    )
 
 def init_llm():
     return load_llm(os.getenv('MODEL_PROVIDER', ''))
